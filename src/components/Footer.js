704652fd8b50464543a07b1101bf9c08837b5164
Reggie Brucales <109843198+thisguyreggscodes@users.noreply.github.com>
2022-09-03 00:50:01 +0800

    Add existing project files prior to the push to GitHub.

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="justify-content-center d-flex">
+        <div className="card-name">
+          <img
+            alt="paymaya"
+            src="https://manilastandard.net/wp-content/uploads/2022/05/maya.jpg"
+          />
+        </div>
+        <div className="card-name">
+          <img
+            alt="gcash"
+            src="https://www.gcash.com/wp-content/uploads/2021/09/Customer-Services-Financial-Services-GCash-Pera-Outlet-GPO-Logo-500x500-1.png"
+          />
+        </div>
+        <div className="card-name">
+          <img
+            alt="paypal"
+            src="https://pbs.twimg.com/media/EfTZlEnWAAMn1lX.png"
+          />
+        
+        </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>Call Us Mondays to Saturdays</h5>
+            <p>+639 553 479 729</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Shop Location</h5>
+            <p>Ligao City, Albay</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-fax"></i>
+            </div>
+            <h5>Office Number</h5>
+            <p>+639 615 454 590</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
